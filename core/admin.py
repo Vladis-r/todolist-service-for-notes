eb113688beb05994dcb9f9cb2b08720f3bc65043
Vladislav <ragozin.vladis@gmail.com>
2022-12-24 16:58:47 +0300

    fix admin-panel

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from core.models import User
 
-admin.site.register(User)
+
+# admin.site.register(User)
+
+
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("email", "first_name", "last_name", "username")
+    fields = ("username", "first_name", "last_name", "email", "is_staff", "is_active", "date_joined", "last_login")
+    readonly_fields = ("date_joined", "last_login")
